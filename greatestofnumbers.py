5950c7f031fa2abb421ef381c630f48f6ce343a1
Vignesh <46440651+Vignesh29071998@users.noreply.github.com>
2019-01-07 09:38:35 +0000

    Update greatestofnumbers.py

--- greatestofnumbers.py
+++ greatestofnumbers.py
@@ -1,3 +1,14 @@
+a=int(input())
+b=int(input())
+c=int(input())
+if a>b and a>c:
+  print(a)
+elif b>a and b>c:
+  print(b)
+else:
+  print(c)
 li=list(map(int,input().split()))
+2
 sorted(li)
+3
 print(li[len(li)-1])
