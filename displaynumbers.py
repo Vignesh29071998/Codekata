bd9f00b7cc37af8af63ad441fbc36a78762ab15f
Vignesh <46440651+Vignesh29071998@users.noreply.github.com>
2019-01-08 11:58:31 +0530

    Update displaynumbers.py

--- displaynumbers.py
+++ displaynumbers.py
@@ -1,5 +1,11 @@
 #hello
 a,b=map(int,input().split())
+l=[]
 for i in range(a+1,b):
   if i%2!=0:
-    print(i,end=' ')
+    l.append(i)
+for j in range(0,len(l)):
+  if j==len(l)-1:
+    print(l[j])
+  else:
+    print(l[j],end=' ')
