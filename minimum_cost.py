9d38137f2bd97d86cc7aed986f56c6c5004adff0
Vignesh <46440651+Vignesh29071998@users.noreply.github.com>
2019-01-11 00:33:39 +0530

    Update minimum_cost.py

--- minimum_cost.py
+++ minimum_cost.py
@@ -1,12 +1,23 @@
 x,y=input().split()
 c=0
-for i in range(0,len(y)):
-  try:
-    if x[i]!=y[i]:
-      x.replace(x[i],y[i])
+if len(x)<len(y):
+  for i in range(0,len(y)):
+    try:
+      if x[i]!=y[i]:
+        x.replace(x[i],y[i])
+        c+=1
+    except IndexError:
+      x+=y[i]
       c+=1
-  except IndexError:
-    x+=y[i]
-    c+=1
-print(c)
+  print(c)
+else:
+  for i in range(0,len(x)):
+    try:
+      if x[i]!=y[i]:
+        x.replace(x[i],y[i])
+        c+=1
+    except IndexError:
+      c+=len(x)-i
+      print(c)
+      break
     
