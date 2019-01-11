a94841927c2731c09bd33a2f34d3fd4f3b75f724
Vignesh <46440651+Vignesh29071998@users.noreply.github.com>
2019-01-11 10:04:19 +0530

    Create check_repetition.py

---
+++ check_repetition.py
@@ -0,0 +1,16 @@
+n=int(input())
+n1=list(map(int,input().split()))
+b=[]
+l=[0]*10
+for i in range(0,len(n1)):
+  l[n1[i]]+=1
+a=max(l)
+for i in range(0,len(l)):
+  if a==l[i]:
+    b.append(i)
+print(b)
+for i in range(0,len(b)):
+  if i==len(b)-1:
+    print(b[i])
+  else:
+    print(b[i],end=' ')
