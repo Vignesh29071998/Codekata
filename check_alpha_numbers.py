2d9b9dad97c4f4c4ec2fb321ca9c949bc10edf67
Vignesh <46440651+Vignesh29071998@users.noreply.github.com>
2019-01-14 14:49:58 +0530

    Update check_alpha_numbers.py

--- check_alpha_numbers.py
+++ check_alpha_numbers.py
@@ -1,6 +1,8 @@
 import re
 n=input()
-if re.search("\w",n) is not None:
-  print('Yes')
-else:
-  print('No')
+if re.search("[a-zA-Z]",n) is not None:
+    if re.search("[0-9]",n) is not None:
+        print('Yes')
+    else:
+        print('No')
+    
