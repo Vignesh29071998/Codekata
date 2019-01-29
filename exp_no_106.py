25caf6911d82ff130df258526543d6e5cad81ed3
Vignesh <46440651+Vignesh29071998@users.noreply.github.com>
2019-01-29 16:12:25 +0530

    Update exp_no_106.py

--- exp_no_106.py
+++ exp_no_106.py
@@ -1,6 +1,10 @@
 N,K=map(int,input().split())
 N1=list(map(int,input().split()))
+N1=sorted(N1)
 if N1[K]%2!=0:
   print(N1[K])
 else:
-  print(N1[K-1])
+  if N1[K+1]%2!=0:
+    print(N1[K+1])
+  elif N1[K-1]%2!=0: 
+    print(N1[K-1])
